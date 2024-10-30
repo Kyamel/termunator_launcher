2be568411903d00cf8af892dec38cecae5f029f8
Kyamel <129009324+Kyamel@users.noreply.github.com>
2024-10-30 15:05:32 -0300

    feat: readabilite

--- games/termunator_nave/src/main.rs
+++ games/termunator_nave/src/main.rs
@@ -1,74 +1,56 @@
 
 use termunator::utils::{clear_area, custom_print};
-use termunator::utils;
 use termunator::KeyCode;
 use termunator::World;
 use termunator::Components::*;
 use termunator::Systems::*;
-use utils::crossterm_interactive;
-
 
 fn main() {
     termunator::hello();
     println!("Hello Nave");
 
-    //crossterm_interactive();
-
+    // Initialize World
     let mut world = World::World::new();
 
-    // Event Handling
+    // Add systems
     let event_system = HandleEventsSystem::new();
-    world.add_system("handle_events", event_system);
-
-    let key = KeyState::new();
-    let player_entity = world.create_entity();
-    world.add_component(player_entity, key);
-
-    // Draw
-
     let draw_system = DrawSystem::new();
+    let player_system = PlayerSystem::new();
+
+    world.add_system("handle_events", event_system);
     world.add_system("draw", draw_system);
+    world.add_system("player", player_system);
 
+    // Initialize player
+    let player_entity = world.create_entity();
+
+    // Add components to player
+    let key = KeyState::new();
     let body = Body::new(
-    vec![
-            vec![' ', '^', '^', ' '],
-            vec!['/', '0', '0', '\\'],
-            vec!['|', '=', '=', '|'],
-            vec![' ', '/', '\\', ' '],
-        ]
-    );
-
+        vec![
+                vec![' ', '^', '^', ' '],
+                vec!['/', '0', '0', '\\'],
+                vec!['|', '=', '=', '|'],
+                vec![' ', '/', '\\', ' '],
+            ]
+        );
     let pos = Position::new(0.0, 5.0);
     let vel = Velocity::new(1.0, 1.0);
 
-    let player_system = PlayerSystem::new();
-    let mut game_state = GameState::new();
-
+    world.add_component(player_entity, key);
     world.add_component(player_entity, body);
     world.add_component(player_entity, pos);
     world.add_component(player_entity, vel);
 
-    world.add_system("player", player_system);
-
-
-    // Movement
-    //let movement_system = MovementSystem::new();
-    //world.add_system("movement", movement_system);
-
+    // Initialize window
+    let mut game_state = GameState::new();
     let window = termunator::World::World::init(16*2, 9).unwrap();
     game_state.window_size = window;
-
     world.add_component(player_entity, game_state);
 
-    let target_fps = 60;
-
-    // Game loop
     'game_loop: loop {
 
         clear_area(window.0, window.1);
-
-        //let mut stdout = std::io::stdout();
-        //execute!(stdout, Clear(ClearType::All)).expect("Erro ao limpar a tela");
 
         let keys = world.get_component::<KeyState>(player_entity).unwrap();
         if keys.is_key_pressed(KeyCode::Char('q')) {
@@ -77,6 +59,5 @@ fn main() {
 
        world.update(60);
     }
-
 
 }
